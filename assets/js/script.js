6dcddb988f7b1c36274bcfc93c5d77ce5a869ffa
deroberts <s501656@gmail.com>
2022-10-24 21:31:37 -0500

    added event listener, updated update favorites and add to favorites functions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 let $movieFavEl = $("#movie-fav");
 let myMovies = ["Movie 1", "Movie 2", "Movie 3", "Movie 4"];
-
+let $searchEl = $("#searchBtn")
 
 function updateFavorites() {
-
+$movieFavEl.empty();
     myMovies.forEach((x) => {
         $movieFavEl.append("<li class=\"listitem\"><div class=\"text\"> " + x + " <i class=\"fa fa-film\"></i></div></li>");
     });
@@ -12,15 +12,17 @@ function updateFavorites() {
 function addToFavorites(event) {
     event.preventDefault()
     let movie;
-    movie = event.target.innerHTML
-    console.log(movie)
+    movie = $(event.target).children("input").val()
+    // console.log(movie)
+    myMovies.push(movie)
+    updateFavorites()
 //grabEventData()
 
 }
 
 updateFavorites();
 
-// document.getElementByIdaddEventListener 
+$searchEl.on('submit', addToFavorites)
 
 
 
